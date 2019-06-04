0ac509cc630f2a06335565299664d43a5e0af326
Jaeho Lee <jaeho.lee@snu.ac.kr>
2019-06-04 16:27:32 +0900

    :zap: Rewrote code using selenium-provided-packages

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
 options.headless = True
@@ -10,22 +11,16 @@ browser = Chrome(options=options)
 browser.get("https://papago.naver.com")
 
 timeout = 10
-start = time.time()
-while time.time() - start < timeout:
-    try:
-        txt_source = browser.find_element_by_id("txtSource")
-        txt_target = browser.find_element_by_id("txtTarget")
-        break
-    except NoSuchElementException:
+try:
+    txt_source = (WebDriverWait(browser, timeout)
+                  .until(EC.presence_of_element_located((By.ID, "txtSource"))))
+    txt_target = (WebDriverWait(browser, timeout)
+                  .until(EC.presence_of_element_located((By.ID, "txtTarget"))))
+
+    to_translate = input("번역할 내용을 입력하세요: ")
+    txt_source.send_keys(to_translate)
+    while txt_target.text == "" or txt_target.text[-3:] == "...":
         continue
-if "txt_source" not in locals() or "txt_target" not in locals():
-    raise TimeoutException(f"Aborted due to a timeout ({timeout}s)")
-
-to_translate = input("번역할 내용을 입력하세요: ")
-txt_source.send_keys(to_translate)
-while txt_target.text == "" or txt_target.text[-3:] == "...":
-    continue
-print("번역된 결과입니다:", txt_target.text)
-
-browser.quit()
-
+    print("번역된 결과입니다:", txt_target.text)
+finally:
+    browser.quit()
